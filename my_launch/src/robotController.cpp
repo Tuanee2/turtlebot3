d1ba37e2c81bfea68157d6a58c8b12594a331d7d
Tuanee2 <buiquoctuan18102002@gmail.com>
2024-05-04 11:31:15 +0700

    new robot controller

--- my_launch/src/robotController.cpp
+++ my_launch/src/robotController.cpp
@@ -2,11 +2,20 @@
 #include "nav_msgs/msg/path.hpp"
 #include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2/LinearMath/Matrix3x3.h"
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <pthread.h>
 #include <signal.h>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <math.h>
+
+bool cond = true;
+
 
 class PlanPoseToCmdVelNode : public rclcpp::Node
 {
@@ -23,6 +32,7 @@ public:
 
     // Publisher for /cmd_vel
     publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+    
     RCLCPP_INFO(this->get_logger(), "Star follow the path.");
   }
 
@@ -31,6 +41,7 @@ private:
   {
     RCLCPP_INFO(this->get_logger(), "Received path with %zu poses", msg->poses.size());
     // You might want to implement some logic based on the path here
+    plan.poses = msg->poses;
   }
 
   void pose_callback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
@@ -38,6 +49,8 @@ private:
     RCLCPP_INFO(this->get_logger(), "Received pose: [%f, %f, %f]",
                 msg->pose.pose.position.x, msg->pose.pose.position.y, msg->pose.pose.position.z);
     // Process the pose information and potentially publish to /cmd_vel
+    current_pose.pose = msg->pose;
+    //
     auto cmd_vel = geometry_msgs::msg::Twist();
     cmd_vel.linear.x = 0.5;  // Example: move forward with a predefined speed
     cmd_vel.angular.z = 0.0; // Example: no rotation
@@ -46,13 +59,87 @@ private:
     RCLCPP_INFO(this->get_logger(), "Published cmd_vel to move forward");
   }
 
-  void controller(){
+  void controller()
+  {
+    size_t target_pose_index_ = 0;
+    geometry_msgs::msg::PoseStamped target_pose;
+    target_pose.header.stamp = current_pose.header.stamp;
 
+    while(target_pose_index_ <= plan.poses.size())
+    {
+      target_pose.pose = plan.poses[target_pose_index_].pose;
+      //error_yaw
+      double roll, pitch;
+      // get current_yaw
+      tf2::Quaternion q_current(
+        current_pose.pose.pose.orientation.x,
+        current_pose.pose.pose.orientation.y,
+        current_pose.pose.pose.orientation.z,
+        current_pose.pose.pose.orientation.w
+      );
+      tf2::Matrix3x3 n(q_current);
+      n.getRPY(roll, pitch, current_yaw);
+      // calculate target_yaw
+      target_yaw = atan2(target_pose.pose.position.y - current_pose.pose.pose.position.y,target_pose.pose.position.x - current_pose.pose.pose.position.x);
+      desire_yaw = target_yaw - current_yaw;
+      // limit desire_yaw
+      if (desire_yaw  > M_PI) {
+        desire_yaw  -= 2 * M_PI;
+      }else if (desire_yaw  < -M_PI) {
+        desire_yaw  += 2 * M_PI;
+      }
+      // P controller
+      if(cond == true){
+        if (desire_yaw >= 0.01 || desire_yaw <= -0.01)
+        {
+          cmd_vel.linear.x = 0;
+          cmd_vel.angular.z = desire_yaw;
+          publisher_->publish(cmd_vel);
+        }else{
+          cmd_vel.linear.x = 0;
+          cmd_vel.angular.z = 0;
+          publisher_->publish(cmd_vel);
+          cond = false;
+        }
+      }
+
+      if(cond == false){
+        if (pose_error <= 0.05 || pose_error >= -0.05)
+        {
+          cmd_vel.linear.x = 0;
+          cmd_vel.angular.z = 0;
+          publisher_->publish(cmd_vel);
+          target_pose_index_++;
+          cond = true;
+        }
+        else
+        {
+          double x_err = (target_pose.pose.position.x - current_pose.pose.pose.position.x);
+          double y_err = (target_pose.pose.position.y - current_pose.pose.pose.position.y);
+          pose_error = sqrt(x_err*x_err + y_err*y_err);
+          cmd_vel.linear.x = Kp_x*pose_error;
+          cmd_vel.angular.z = desire_yaw;
+          publisher_->publish(cmd_vel);
+        }
+      }
+    
+      if (target_pose_index_ > plan.poses.size()) 
+      {
+        RCLCPP_INFO(this->get_logger(), "Wait a new path!");
+        break;
+      }
+    }
   }
 
   rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr subscription_plan_;
   rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr subscription_pose_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  geometry_msgs::msg::Twist cmd_vel;
+  geometry_msgs::msg::PoseWithCovarianceStamped current_pose;
+  nav_msgs::msg::Path plan;
+  rclcpp::TimerBase::SharedPtr timer_;
+  double current_yaw , target_yaw, desire_yaw, pose_error;
+  float Kp_x = 3;
 };
 
 int main(int argc, char *argv[])
