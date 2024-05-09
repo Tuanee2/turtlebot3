d5dec1a912c34781c2cbed7451288f6bf0c8eb45
Tuanee2 <buiquoctuan18102002@gmail.com>
2024-05-09 13:06:04 +0700

    feat: Add workspace creation feature

--- my_launch/rviz_plugin/include/myCustomDisplay.hpp
+++ my_launch/rviz_plugin/include/myCustomDisplay.hpp
@@ -1,6 +1,7 @@
 #ifndef MY_CUSTOM_DISPLAY_HPP_
 #define MY_CUSTOM_DISPLAY_HPP_
 
+#include <memory>
 #include <rviz_common/display.hpp>
 #include <rviz_common/ros_topic_display.hpp>
 #include <rviz_common/properties/color_property.hpp>
@@ -28,17 +29,25 @@ protected:
   virtual void subscribe() override;
   virtual void unsubscribe() override;
   virtual void processMessage(const geometry_msgs::msg::PointStamped::ConstSharedPtr msg) override;
-  virtual void connectPoints();
 
 private:
-  rviz_common::properties::ColorProperty* color_property_;
-  rviz_common::properties::IntProperty* size_property_;
-  std::vector<std::pair<Ogre::SceneNode*, Ogre::ManualObject*>> points_;
-  std::vector<Ogre::Vector3> point_positions_;
-  std::vector<Ogre::ManualObject*> lines_;
+  void createPoint(const geometry_msgs::msg::Point& point,const std::string& type);
+  void connectPoints(const std::string& type);
+  void drawCurvedLine(Ogre::ManualObject* line, const Ogre::Vector3& start, const Ogre::Vector3& end);
+  Ogre::MaterialPtr createOrGetPointMaterial(const std::string& type);
+  Ogre::MaterialPtr createOrGetLineMaterial(const std::string& type);
+  std::unique_ptr<rviz_common::properties::ColorProperty> color_property_;
+  std::unique_ptr<rviz_common::properties::IntProperty> size_property_;
+  std::unique_ptr<rviz_common::properties::EnumProperty> line_type_property_;
+  std::unique_ptr<rviz_common::properties::EnumProperty> line_typeOfShape_property_;
+  std::unique_ptr<rviz_common::properties::EnumProperty> line_Orientation_type_property_;
+  std::unique_ptr<rviz_common::properties::FloatProperty> radius_property_;
   Ogre::MaterialPtr line_material_;
-  rviz_common::properties::EnumProperty* line_type_property_;
-  rviz_common::properties::FloatProperty* radius_property_;
+  std::vector<Ogre::Vector3> point_positions_path;
+  std::vector<Ogre::Vector3> point_positions_wall;
+  std::map<Ogre::SceneNode*, Ogre::ManualObject*> points_;
+  std::vector<Ogre::ManualObject*> lines_path;
+  std::vector<Ogre::ManualObject*> lines_wall;
 };
 
 #endif // MY_CUSTOM_DISPLAY_HPP_
--- my_launch/rviz_plugin/src/myCustomDisplay.cpp
+++ my_launch/rviz_plugin/src/myCustomDisplay.cpp
@@ -1,152 +1,248 @@
-#include "myCustomDisplay.hpp"
 #include <OgreSceneManager.h>
 #include <OgreSceneNode.h>
 #include <OgreManualObject.h>
 #include <OgreMaterialManager.h>
 #include <OgreTechnique.h>
+#include "myCustomDisplay.hpp"
 #include <vector>
 #include <iostream>
+#include <memory> // For smart pointers
 #include <math.h>
 
 using namespace rviz_common;
 
-MyCustomDisplay::MyCustomDisplay() {
-  color_property_ = new properties::ColorProperty("Color", QColor(255, 0, 0),
-                                                  "Color of the point.", this);
-  size_property_ = new properties::IntProperty("Size", 10,
-                                               "Size of the point.", this);
-  line_type_property_ = new properties::EnumProperty("Line Type", "Straight",
-                                                     "Choose between a straight line or a curved line.", this);
-  line_type_property_->addOption("Straight", 0);
-  line_type_property_->addOption("Curved", 1);
-  radius_property_ = new properties::FloatProperty("Radius", 1.0,
-                                                   "Radius of the curve (only used if 'Curved' is selected).", this);
-
+MyCustomDisplay::MyCustomDisplay() :
+    color_property_(new properties::ColorProperty("Color", QColor(255, 0, 0),
+                                                  "Color of the point.", this)),
+    size_property_(new properties::IntProperty("Size", 10,
+                                               "Size of the point.", this)),
+    line_type_property_(new properties::EnumProperty("Line Type","Path","Choose between wall or path.",this)),
+    line_typeOfShape_property_(new properties::EnumProperty("Line Type of shape", "Straight",
+                                                     "Choose between a straight line or a curved line.", this)),
+    line_Orientation_type_property_(new properties::EnumProperty("Orientation Type", "UpsideDown",
+                                                                 "Choose between a UpsideDown type or UpRight type.",this)),
+    radius_property_(new properties::FloatProperty("Radius", 1.0,
+                                                   "Radius of the curve (only used if 'Curved' is selected).", this))
+{
+    line_type_property_->addOption("Path",0);
+    line_type_property_->addOption("Wall",1);
+    line_typeOfShape_property_->addOption("Straight", 0);
+    line_typeOfShape_property_->addOption("Curved", 1);
+    line_Orientation_type_property_->addOption("UpsideDown", 0);
+    line_Orientation_type_property_->addOption("UpRight", 1);
 }
 
 MyCustomDisplay::~MyCustomDisplay() {
-  for (auto& entry : points_) {
-    scene_manager_->destroyManualObject(entry.second);
-    scene_node_->removeAndDestroyChild(entry.first);
-  }
-  // Delete the properties if they were created with new
-  delete color_property_;
-  delete size_property_;
-  delete line_type_property_;
-  delete radius_property_;
+    for (auto& entry : points_) {
+        scene_manager_->destroyManualObject(entry.second);
+        scene_node_->removeAndDestroyChild(entry.first);
+    }
 }
 
 void MyCustomDisplay::onInitialize() {
-  RosTopicDisplay::onInitialize();
+    RosTopicDisplay::onInitialize();
 }
 
 void MyCustomDisplay::onEnable() {
-  subscribe();
+    subscribe();
 }
 
 void MyCustomDisplay::onDisable() {
-  unsubscribe();
+    unsubscribe();
 }
 
 void MyCustomDisplay::subscribe() {
-  RosTopicDisplay::subscribe();
+    RosTopicDisplay::subscribe();
 }
 
 void MyCustomDisplay::unsubscribe() {
-  RosTopicDisplay::unsubscribe();
+    RosTopicDisplay::unsubscribe();
 }
 
-// main process
 void MyCustomDisplay::processMessage(const geometry_msgs::msg::PointStamped::ConstSharedPtr msg) {
-  if (points_.size() >= 100) {
-    std::cout << "Đã đạt tối đa số điểm có thể nhấn là 100." << std::endl;
-    return; // Ngừng xử lý và không thêm điểm mới
-  }
-  // add a new point position to "point_position_" Vector
-  point_positions_.push_back(Ogre::Vector3(msg->point.x, msg->point.y, 0));
-  Ogre::SceneNode* node = scene_node_->createChildSceneNode();
-  Ogre::ManualObject* point = scene_manager_->createManualObject();
-  Ogre::MaterialPtr material;
-  if (Ogre::MaterialManager::getSingleton().resourceExists("PointMaterial")) {
-      material = Ogre::MaterialManager::getSingleton().getByName("PointMaterial");
-  } else {
-      material = Ogre::MaterialManager::getSingleton().create(
-          "PointMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-      material->setReceiveShadows(false);
-      material->getTechnique(0)->getPass(0)->setDiffuse(color_property_->getOgreColor());
-      material->getTechnique(0)->getPass(0)->setAmbient(color_property_->getOgreColor());
-      material->getTechnique(0)->getPass(0)->setPointSize(size_property_->getInt());
-
-      line_material_ = Ogre::MaterialManager::getSingleton().create(
-          "LineMaterial", Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
-      line_material_->setReceiveShadows(false);
-      // Bạn có thể tùy chỉnh các thuộc tính hiển thị của đường tại đây (ví dụ như màu sắc, độ dày)
-      line_material_->getTechnique(0)->getPass(0)->setDiffuse(Ogre::ColourValue(0.0f, 1.0f, 0.0f)); 
-      line_material_->getTechnique(0)->getPass(0)->setLineWidth(5.0f); // Độ dày đường là 2
-  }
-  point->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_POINT_LIST);
-  point->position(msg->point.x, msg->point.y, 0);
-  point->colour(color_property_->getOgreColor());
-  point->end();
-  point->setMaterial(0, material);
-  node->attachObject(point);
-  MyCustomDisplay::connectPoints();
-}
-
-void MyCustomDisplay::connectPoints() {
-  // Method to connect points based on the selected line type and configuration
-  // Clear existing lines (if any)
-  for (const auto& line : lines_) {
-    scene_manager_->destroyManualObject(line);
-  }
-  lines_.clear();
-
-  // Check if there are enough points (at least 2) to draw a line
-  if (point_positions_.size() < 2) {
-    return;
-  }
-
-  // Iterate through consecutive pairs of points
-  for (size_t i = 0; i < point_positions_.size() - 1; ++i) {
-    const Ogre::Vector3& p1 = point_positions_[i];
-    const Ogre::Vector3& p2 = point_positions_[i + 1];
-
-    Ogre::ManualObject* line = scene_manager_->createManualObject();
-    line->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_LINE_LIST);
-
-    if (line_type_property_->getOptionInt() == 0) { // Straight line
-      line->position(p1);
-      line->position(p2);
-    } else { // Curved line (assuming a simple arc)
-      Ogre::Real radius = radius_property_->getFloat();
-      Ogre::Real radius1 =  sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y)) + radius;
-      Ogre::Vector3 center = (p1 + p2) / 2.0f;
-      Ogre::Vector3 direction = p2 - p1;
-      direction.normalise();
-
-      // Calculate start and end points for the arc based on direction
-      Ogre::Vector3 start_point = center + direction.perpendicular() * radius1;
-      Ogre::Vector3 end_point = center - direction.perpendicular() * radius1;
-
-      // Draw an arc with appropriate number of segments
-      const int segments = 20; // Adjust as needed for smoothness
-      for (int j = 0; j <= segments; ++j) {
-        Ogre::Real angle = j * M_PI / segments;
-        Ogre::Vector3 point = center + radius * direction.perpendicular() * cos(angle) + radius * direction.crossProduct(direction.perpendicular()) * sin(angle);
-        line->position(point);
-      }
+    if (points_.size() >= 100) {
+        std::cerr << "Maximum number of points (100) reached." << std::endl;
+        return;
+    }
+    
+    if (!msg) {
+        std::cerr << "Received invalid message." << std::endl;
+        return;
     }
 
-    line->colour(color_property_->getOgreColor());
-    line->end();
+    
+    std::string type;
+    if(line_type_property_->getOptionInt() == 0){
+        type = "Path";
+        point_positions_path.push_back(Ogre::Vector3(msg->point.x, msg->point.y, 0));
+    }else{
+        type = "Wall";
+        point_positions_wall.push_back(Ogre::Vector3(msg->point.x, msg->point.y, 0));
+    }
+    
+    
+    createPoint(msg->point,type);
+    connectPoints(type);
+}
 
-    // Set material and attach to scene node
-    line->setMaterial(0, line_material_); // Assuming a shared material for lines
-    scene_node_->attachObject(line);
-    lines_.push_back(line);
-  }
+void MyCustomDisplay::createPoint(const geometry_msgs::msg::Point& point, const std::string& type) {
+    Ogre::SceneNode* node = scene_node_->createChildSceneNode();
+    Ogre::ManualObject* pointObj = scene_manager_->createManualObject();
+    
+    pointObj->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_POINT_LIST);
+    pointObj->position(point.x, point.y, 0);
+    pointObj->colour(color_property_->getOgreColor());
+    pointObj->end();
+    
+    pointObj->setMaterial(0, createOrGetPointMaterial(type));
+    node->attachObject(pointObj);
+    points_[node] = pointObj;
+}
+
+Ogre::MaterialPtr MyCustomDisplay::createOrGetPointMaterial(const std::string& type) {
+    std::string materialName = "PointMaterial_" + type;
+    if (Ogre::MaterialManager::getSingleton().resourceExists(materialName)) {
+        return Ogre::MaterialManager::getSingleton().getByName(materialName);
+    } else {
+        Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().create(
+            materialName, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+        material->setReceiveShadows(false);
+        material->getTechnique(0)->getPass(0)->setDiffuse(color_property_->getOgreColor());
+        material->getTechnique(0)->getPass(0)->setAmbient(color_property_->getOgreColor());
+        material->getTechnique(0)->getPass(0)->setPointSize(size_property_->getInt());
+        return material;
+    }
+}
+
+void MyCustomDisplay::connectPoints(const std::string& type) {
+
+    std::vector<Ogre::Vector3>& points = (type == "Path") ? point_positions_path : point_positions_wall;
+    std::vector<Ogre::ManualObject*>& lines = (type == "Path") ? lines_path : lines_wall;
+
+    if (points.size() < 2) {
+        return;
+    }
+
+    for (const auto& line : lines) {
+        scene_manager_->destroyManualObject(line);
+    }
+    lines.clear();
+
+    for (size_t i = 0; i < points.size() - 1; ++i) {
+        const Ogre::Vector3& p1 = points[i];
+        const Ogre::Vector3& p2 = points[i + 1];
+        
+        Ogre::ManualObject* line = scene_manager_->createManualObject();
+        line->begin("BaseWhiteNoLighting", Ogre::RenderOperation::OT_LINE_LIST);
+
+        if (line_typeOfShape_property_->getOptionInt() == 0) { // Straight line
+            line->position(p1);
+            line->position(p2);
+            
+        } else { // Curved line
+            drawCurvedLine(line, p1, p2);
+        }
+
+        line->colour(color_property_->getOgreColor());
+        line->end();
+        
+        line->setMaterial(0, createOrGetLineMaterial(type));
+        scene_node_->attachObject(line);
+        lines.push_back(line);
+    }
+}
+
+void MyCustomDisplay::drawCurvedLine(Ogre::ManualObject* line, const Ogre::Vector3& start, const Ogre::Vector3& end) {
+    Ogre::Real radius = radius_property_->getFloat();
+    Ogre::Real distance = start.distance(end);
+    Ogre::Vector3 mid_point = (start + end) / 2.0f;
+    Ogre::Real real_radius = Ogre::Math::Sqrt(radius * radius + distance * distance/4);
+    Ogre::Radian angle_radian = Ogre::Math::ACos(radius/real_radius);
+    Ogre::Real angle_inc = angle_radian.valueRadians() / 20.0f;
+    // Tính toán tâm của cung tròn
+    Ogre::Vector3 direction = end - start;
+    direction.normalise();
+    Ogre::Vector3 center;
+    if(line_Orientation_type_property_->getOptionInt() == 0 ){
+      center = mid_point + radius * direction.crossProduct(Ogre::Vector3::UNIT_Z);
+    }else{
+      center = mid_point - radius * direction.crossProduct(Ogre::Vector3::UNIT_Z);
+    }
+
+    Ogre::Vector3 vec_from_center_to_start = start - center;
+    Ogre::Vector3 vec_from_center_to_end = end - center;
+    std::cout<<std::endl;
+    std::cout<<"vec_start : "<<vec_from_center_to_start.x <<" : "<<vec_from_center_to_start.y<<std::endl;
+    std::cout<<"vec_end : "<<vec_from_center_to_end.x <<" : "<<vec_from_center_to_end.y<<std::endl;
+    
+    vec_from_center_to_start.normalise();
+    vec_from_center_to_end.normalise();
+
+    // Tính góc giữa hai vectơ và trục x bằng hàm ACos
+    Ogre::Radian angle_start_to_center_x = Ogre::Math::ATan(vec_from_center_to_start.y/vec_from_center_to_start.x);
+    Ogre::Radian angle_end_to_center_x = Ogre::Math::ATan(vec_from_center_to_end.y/vec_from_center_to_end.x);
+    std::cout<<"test"<<std::endl;
+    Ogre::Real angle_start = angle_start_to_center_x.valueRadians();
+    Ogre::Real angle_end = angle_end_to_center_x.valueRadians();
+    std::cout<<"test01"<<std::endl;
+    //**********debug*********
+    std::cout<<start.x <<" : "<<end.x<<std::endl;
+    std::cout<<"Center : "<<center.x <<" : "<<center.y<<std::endl;
+    std::cout<<"vec_start : "<<vec_from_center_to_start.x <<" : "<<vec_from_center_to_start.y<<std::endl;
+    std::cout<<"vec_end : "<<vec_from_center_to_end.x <<" : "<<vec_from_center_to_end.y<<std::endl;
+    std::cout<<angle_start*180/Ogre::Math::PI<<" : "<<angle_end*180/Ogre::Math::PI<<std::endl;
+    //*************************************
+    Ogre::Real angle_swip = 0.0f;
+    if(((angle_end - angle_start) <= Ogre::Math::PI) || ((angle_end - angle_start) >= -Ogre::Math::PI)){
+      if(angle_end - angle_start > 0){
+
+      }else{
+        angle_swip = angle_end;
+        angle_end = angle_start;
+        angle_start = angle_swip;
+      }
+    }else{
+      if(angle_end - angle_start > 0){
+        angle_swip = angle_end;
+        angle_end = angle_start;
+        angle_start = angle_swip;
+      }else{
+
+      }
+    }
+  
+    std::cout<<angle_start*180/Ogre::Math::PI<<" : "<<angle_end*180/Ogre::Math::PI<<std::endl;
+        
+    for (Ogre::Real angle = angle_start; angle <= angle_end ; angle += angle_inc) {
+        Ogre::Real x = center.x + real_radius * Ogre::Math::Cos(angle);
+        Ogre::Real y = center.y + real_radius * Ogre::Math::Sin(angle);
+        Ogre::Real z = center.z;
+        Ogre::Vector3 point(x, y, z);
+          // Thêm điểm vào đường nối
+        line->position(point);
+    }
+    
+
+}
+
+Ogre::MaterialPtr MyCustomDisplay::createOrGetLineMaterial(const std::string& type) {
+    std::string materialName = "LineMaterial_" + type;
+
+    if (Ogre::MaterialManager::getSingleton().resourceExists(materialName)) {
+        return Ogre::MaterialManager::getSingleton().getByName(materialName);
+    }
+
+    Ogre::MaterialPtr material = Ogre::MaterialManager::getSingleton().create(
+        materialName, Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME);
+    material->setReceiveShadows(false);
+    if(type == "Wall"){
+        material->getTechnique(0)->getPass(0)->setDiffuse(Ogre::ColourValue(1.0f, 0.0f, 0.0f));
+    }else{
+        material->getTechnique(0)->getPass(0)->setDiffuse(Ogre::ColourValue(0.0f, 1.0f, 0.0f));
+    }
+    material->getTechnique(0)->getPass(0)->setLineWidth(20.0f);
+    return material;
 }
 
 #include <pluginlib/class_list_macros.hpp>
 PLUGINLIB_EXPORT_CLASS(MyCustomDisplay, rviz_common::Display)
-
